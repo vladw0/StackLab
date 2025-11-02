ba1e9a82591550a3bb61eb117e98945c3a956bf9
vladw0 <bobvolston@gmail.com>
2025-11-02 22:21:50 +0300

    changes in stack.h. Added SetData and Push methods.

--- include/stack.h
+++ include/stack.h
@@ -77,13 +77,27 @@ inline TStack<T>::TStack(int len_){
 } 
 
 template<class T>
-inline TStack<T>::TStack(const TStack& obj) : TStack<T>::TStack(){
+inline TStack<T>::TStack(const TStack& obj){
+    data = nullptr;
+    len = 0;
+    top = 0;
+    isNew = true;
     if (obj.len < 0){
+        throw -1;
+    }
+    if (obj.len > 0){
         len = obj.len;
-        data = new T * [len];
+        data = new T*[len];
         for (int i = 0; i < len; i++){
-            data[i] = new * T (obj.data[i]);
+            if (i < obj.top && obj.data[i] != 0){
+                data[i] = new T(*(obj.data[i]));
+            }
+            else {
+                data[i] = nullptr;
+            }    
         }
+        top = obj.top;
+        isNew = true;
     }
 }
 
@@ -92,43 +106,114 @@ inline TStack<T>::TStack(TStack&& obj){
     len = obj.len;
     data = obj.data;
     isNew = obj.isNew;
+    top = obj.top;
 
     obj.len = 0;
     obj.data = nullptr;
     obj.isNew = false;
+    obj.top = 0;
 
 }
 
 template<class T>
-inline TStack<T>::TStack(T** data_, int len_) : TStack<t>::TStack(){
+inline TStack<T>::TStack(T** data_, int len_){
     if (len < 0){
         throw -1;
     }
-    else if (len > 0){
-        len = len_;
-        data = data_;
-        isNew = false;
-        for (int i = 0; i < len; i++){
-            if (data[i] == nullptr){
-                top = i;
-                break;
-            }
+    data = data_;
+    len = len_;
+    isNew = false;
+    top = 0;
+    for (int i = 0; i < len; i++){
+        if (data[i] == 0){
+            top = i;
+            break;
         }
+        top = i + 1;
     }
 }
 
 template<class T>
 inline TStack<T>::~TStack(){
-    if (isNew){
-        if (data != nullptr){
-            for (int i = 0; i < len; i++){
+    if (isNew && data != nullptr){
+        for (int i = 0; i < len; i++){
+            if (data[i] != nullptr){
                 delete data[i];
+                data[i] = 0;
             }
         }
+        delete [] data;
+        data = nullptr;
     }
 } 
 
 template<class T>
-inline int TStack<T>::GetLen(){
-    return 0;
+inline int TStack<T>::GetLen() const{
+    return len;
+}
+
+template <class T>
+inline int TStack<T>::GetTop() const{ 
+    return top; 
+}
+
+template <class T>
+inline void TStack<T>::Resize(int len_) {
+    if (!isNew){ 
+        throw -1; 
+    }
+    if (len_ < 0){ 
+        throw -1; 
+    }
+    if (len_ == len){
+        return;
+    }
+    T** newdata = nullptr;
+    if (len_ > 0){
+        newdata = new T*[len_];
+        for (int i = 0; i < len_; i++){ 
+            newdata[i] = 0; 
+        }
+        int limit = len_ < len ? len_ : len;
+        for (int i = 0; i < limit; i++){ 
+            newdata[i] = data[i]; 
+        }
+    }
+    if (data != 0){ 
+        delete [] data; 
+    }
+    data = newdata;
+    len = len_;
+    if (top > len){ 
+        top = len; 
+    }
+}
+
+template <class T>
+inline void TStack<T>::SetData(T** data_, int len_) {
+    if (isNew && data != nullptr) {
+        for (int i = 0; i < len; ++i) {
+            if (data[i] != nullptr) { 
+                delete data[i]; 
+                data[i] = nullptr; 
+            }
+        }
+        delete [] data;
+    }
+    data = data_; 
+    len = len_; 
+    isNew = false; 
+    top = 0;
+    for (int i = 0; i < len; ++i) {
+        if (data[i] == 0) { top = i; break; }
+        top = i + 1;
+    }
+}
+
+template <class T>
+inline void TStack<T>::Push(T* value){
+    if (top >= len) { 
+        throw -1; 
+    }
+    data[top++] = value;
 }
